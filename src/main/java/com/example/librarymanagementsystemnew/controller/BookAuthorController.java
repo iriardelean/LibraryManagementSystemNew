489e8f2f27efec46bebc6be238bf8319d60b9ec3
hudea <hudealex1@gmail.com>
2025-11-03 15:39:11 +0200

    suntem prosti pt. 2

--- src/main/java/com/example/librarymanagementsystemnew/controller/BookAuthorController.java
+++ src/main/java/com/example/librarymanagementsystemnew/controller/BookAuthorController.java
@@ -1,4 +1,4 @@
-package com.example.librarymanagementsystem.controller;
+package com.example.librarymanagementsystemnew.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
