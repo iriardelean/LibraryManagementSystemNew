a35a9c2101b186fd91745b0b2062632821d20a0a
iriardelean <iriardelean33@gmail.com>
2025-11-04 10:34:23 +0200

    added dummy data

--- src/main/java/com/example/librarymanagementsystemnew/repository/ReservationRepository.java
+++ src/main/java/com/example/librarymanagementsystemnew/repository/ReservationRepository.java
@@ -1,9 +1,53 @@
 package com.example.librarymanagementsystemnew.repository;
 import com.example.librarymanagementsystemnew.model.Reservation;
+import com.example.librarymanagementsystemnew.model.ReservationStatus;
+
+import java.util.*;
 
 public class ReservationRepository extends InMemoryRepository<Reservation, String> {
 
     public ReservationRepository() {
         super(Reservation::getId);
+
+        String member1Id = UUID.randomUUID().toString();
+        String member2Id = UUID.randomUUID().toString();
+        String item1Id = UUID.randomUUID().toString();
+        String item2Id = UUID.randomUUID().toString();
+        String item3Id = UUID.randomUUID().toString();
+
+        Reservation res1 = new Reservation(
+                UUID.randomUUID().toString(),
+                member1Id,
+                item1Id,
+                new Date()
+        );
+
+        Reservation res2 = new Reservation(
+                UUID.randomUUID().toString(),
+                member2Id,
+                item2Id,
+                new Date()
+        );
+
+        Reservation res3 = new Reservation(
+                UUID.randomUUID().toString(),
+                member1Id,
+                item3Id,
+                new Date()
+        );
+        res3.setStatus(ReservationStatus.COMPLETED);
+
+        Reservation res4 = new Reservation(
+                UUID.randomUUID().toString(),
+                member2Id,
+                item1Id,
+                new Date()
+        );
+        res4.setStatus(ReservationStatus.CANCELLED);
+
+        save(res1);
+        save(res2);
+        save(res3);
+        save(res4);
     }
 }
